9e86680d3db4ada2869625a88b882162cfd1889f
Adam Beck <abeck@zendesk.com>
2016-12-27 21:41:41 +1100

    0.0.1 -> 0.0.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Spyanator.Mixfile do
   """
   def project do
     [app: :spyanator,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.3",
      name: "Spyanator",
      description: @description,
